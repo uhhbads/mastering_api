477153ccd7a8d93937adc34dba414e963c1d009b
uhhbads <abadshoodee@gmail.com>
2025-12-29 00:15:00 +0800

    added more api req on users.js

--- rest/myapp/routes/users.js
+++ rest/myapp/routes/users.js
@@ -6,4 +6,39 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// POST
+router.post('/', (req, res) => {
+  /*
+  res.status(201).json({
+    message: "User created successfully",
+    data: req.body
+  });
+  */
+
+  console.log("BODY RECEIVED:", req.body);
+
+  res.json({
+    message: "Data successfully received",
+    data: req.body
+  });
+
+});
+
+// PUT
+router.put('/:id', (req, res) => {
+  res.json({
+    message: "User updated",
+    id: req.params.id,
+    data: req.body
+  });
+});
+
+// DELETE
+router.delete('/:id', (req, res) => {
+  res.json({
+    message: "User deleted",
+    id: req.params.id
+  });
+});
+
 module.exports = router;
